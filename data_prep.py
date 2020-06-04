9b372d2363cb244ff31a7dec4d625acb76a3c320
Zeeshan <zhabib3@ufl.edu>
2020-06-03 20:12:19 -0500

    Added filters for missing districts and tos

--- data_prep.py
+++ data_prep.py
@@ -4,25 +4,69 @@ from school import School
 RAW_DATASET = 'raw_school_sites.csv'
 TOS_DATASET = 'District Schools Terms of Service.csv'
 PREPARED_DATASET = 'scraping-sites.csv'
+MISSING_DISTRICTS = 'DistrictsMissing.txt'
 
 
 def main():
     '''This script sanitizes and prepares the dataset for scraping'''
 
     allowed_district_names = filter_district_tos()
-    sanitized_district_names = sanitize_names(allowed_district_names)
-    # print('Sanitized names: ', sanitized_district_names)
-    allowed_sites_list = []
+    allowed_district_names = sanitize_names(allowed_district_names)
     raw_sites_list = read_raw_dataset()
-    write_prepared_dataset(raw_sites_list)
+    filtered_sites_list = filter_by_tos(raw_sites_list, allowed_district_names)
+    filtered_sites_list = filter_by_missing(filtered_sites_list)
+    write_prepared_dataset(filtered_sites_list)
 
 
-def write_prepared_dataset(raw_sites_list):
+def filter_by_tos(raw_school_sites, allowed_district_names):
+    '''Filters out schools which don't allow scraping
+    Returns
+    list: fitered school sites, 
+    '''
+    non_matching_districts = set()
+    filtered_sites = []
+    allowed_set = set(allowed_district_names)
+    allowed_set.update(
+        ['dade', 'fau lab sch', 'uf lab sch', 'fsu lab sch', 'famu lab sch'])
+    for site in raw_school_sites:
+        raw_district_name = site[1].lower()
+        if raw_district_name in allowed_set:
+            filtered_sites.append(site)
+        else:
+            non_matching_districts.add(raw_district_name)
+    print('non matching districts', non_matching_districts)
+    return filtered_sites
+
+
+def filter_by_missing(school_sites):
+    '''Filters out school entries who sent their records (3 districts left for testing)'''
+    missing_set = set()
+    non_missing = set()
+    filtered_sites = []
+    with open(MISSING_DISTRICTS, mode='r') as missing_f:
+        for missing_district in missing_f:
+            missing_set.add(missing_district.lower().strip())
+    missing_set.add('dade')
+    # print("Missing districts ==>", missing_set)
+    
+    for site in school_sites:
+        district_name = site[1].lower()
+        if district_name in missing_set:
+            filtered_sites.append(site)
+        else:
+            non_missing.add(district_name)
+    # print('Non-missing districts: ', non_missing)
+    return filtered_sites
+
+def write_prepared_dataset(sites_list):
     with open(PREPARED_DATASET, mode='w') as prepared_f:
         writer = csv.writer(prepared_f, delimiter=',')
-        writer.writerow(['district','district_name','school','school_name','year','website','has sro (0/1)'])
-        writer.writerows(raw_sites_list)
-            
+        writer.writerow(['district', 'district_name', 'school',
+                         'school_name', 'year', 'website', 'has sro (0/1)'])
+        writer.writerows(sites_list)
+    print(
+        f'Wrote {len(sites_list)} school sites in scraping-sites.csv after filtering')
+
 
 def read_raw_dataset():
     '''Reads raw dataset csv file only parsing single instances of each school
@@ -43,7 +87,7 @@ def read_raw_dataset():
                 continue
             else:
                 line[5] = line[5].strip()
-                line[4] = '2019' # Currently defaulting to latest year
+                line[4] = '2019'  # Currently defaulting to latest year
                 school_names_set.add(line[3])
                 raw_sites_list.append(line)
                 count += 1
@@ -94,6 +138,8 @@ def sanitize_names(district_name_arr):
             error_list.append(name)
     if len(error_list) > 0:
         print(f'Unable to sanitize name for: {error_list}')
+    sanitized_list.remove('gulf')
+    sanitized_list.remove('okeechobee')
     return sanitized_list
 
 
