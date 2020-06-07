4377bcdfc4d98a11945ed179d482290e9af55c22
Zeeshan <zhabib3@ufl.edu>
2020-06-07 15:54:48 -0500

    Added logic for finding staff dir links then navigating to the page and saving it locally

--- school_scraper/school_scraper/spiders/school_spider.py
+++ school_scraper/school_scraper/spiders/school_spider.py
@@ -1,7 +1,9 @@
 import scrapy
-from bs4 import BeautifulSoup
 import re
-""" 
+import csv
+from ..items import School
+from bs4 import BeautifulSoup
+"""
 Staff Dir link found in:
 - Faculty/Staff in Nav -> Staff dir
 - About / About us : hover -> Staff Directory
@@ -11,38 +13,68 @@ response.xpath("//*[contains(text(), 'MY TEXT')]")
 
 regex
 /\bstaff|faculty\b/gi
-"""
 
-"""
 Sites with staff element on home page
     'http://gablese.dadeschools.net/',
     'http://pinecrestacademysouth.dadeschools.net/'
 
 """
 
+SCHOOL_SITES_FILE = 'scraping-sites.csv'
+
 
 class SchoolSpider(scrapy.Spider):
+    schools_list = []
     name = 'school'
     start_urls = [
-        'http://gablese.dadeschools.net/',
-        'http://pinecrestacademysouth.dadeschools.net/'
+        'http://quotes.toscrape.com/tag/humor/',
+        # 'http://gablese.dadeschools.net/',
+        # 'http://pinecrestacademysouth.dadeschools.net/'
     ]
 
+    def get_school_urls(self):
+        schools_list = []
+        count = 0
+        with open(SCHOOL_SITES_FILE) as file:
+            reader = csv.reader(file, delimiter=",")
+            for line in reader:
+                if (count == 0):
+                    count += 1
+                    continue
+                school = School(*line)
+                schools_list.append(school)
+        return schools_list
+        # Return 10 random ones for now
+
     def parse(self, response):
+        self.schools_list = self.get_school_urls()
+        print(self.schools_list)
         soup = BeautifulSoup(response.text, 'lxml')
-        for element in soup.find_all('a', text=re.compile(r"staff|faculty", re.IGNORECASE)):
-            yield {
-                'site_name': response.request.url,
-                'staff_element': element.parent
-            }
-        # staff_xpath = response.xpath(
-        # "//*[contains(text(), 'Staff') or contains(text(), 'Faculty')]")
-        # staff_dir_link = staff_xpath.get()
+        # for element in soup.find_all('a', text=re.compile(r"staff|faculty|directory|employee", re.IGNORECASE)):
+        #     # Grab href link to staff dir
+        #     dir_href = None
+        #     try:
+        #         dir_href = element['href']
+        #     except KeyError:
+        #         print('Error: href attribute not found')
 
-    def write_to_file(self, data, filename):
-        with open(filename, 'wb') as f:
-            f.write(data)
-        self.log(f'Saved file {filename}')
+        #     if dir_href is not None:
+        #         dir_page = response.urljoin(dir_href)
+        #         self.school_name = 'Test'
+        #         yield scrapy.Request(dir_page, callback=self.save_page)
 
-    # def get_random_sites(num):
-    #     with open('scraping-sites.csv', mode='r'):
+        #     yield {
+        #         'site_name': response.request.url,
+        #         'staff_dir_href': dir_href
+        #     }
+
+    def save_page(self, response):
+        try:
+            with open(f'{self.school_name}.html', mode='w+') as file:
+                file.write(response.text)
+            self.log(
+                f'Saved file {self.school_name} from url {response.request.url}')
+            self.school_name += '1'
+        except TypeError:
+            print(
+                'TypeError occured when saving response to file for {response.request.url}')
